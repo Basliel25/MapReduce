0ee3312f9deb8388c708acf781cabd0e9ad5f6d9
Ras Bas <basgug25@gmail.com>
2026-04-29 20:19:02 +0200

    Added insertion sort for MREmit

--- src/MapReduce.c
+++ src/MapReduce.c
@@ -5,46 +5,48 @@ static int num_partitions;
 static Partitioner partitioner;
 
 void MR_Emit(char *key, char *value) {
-
     int partition_num = partitioner(key, num_partitions);
-    
-    // Acquire Lock for partition
+    //Acquire hastable lock
     pthread_mutex_lock(&partitions[partition_num].partition_lock);
 
-    // Access Appropriate bucket
     int bucket_num = partitioner(key, partitions[partition_num].num_buckets);
-    entry_t *entry = partitions[partition_num].buckets[bucket_num];
-    // In  the case that bucket is populated and Key-Value pair exists
-    while(entry != NULL) {
-        if(strcmp(entry->key, key) == 0) {
-            //Create new Node and add value
-            kv_node_t *new_entry = malloc(sizeof(kv_node_t));
-            new_entry->value = strdup(value);
-            new_entry->next = NULL;
-
-            // Crawl to end of list and append
-            kv_node_t *current = entry->values;
-            while(current->next != NULL) current = current->next;
-            current->next = new_entry;
+    entry_t **current_entry_ptr = &partitions[partition_num].buckets[bucket_num];
+    
+    // Key search, if not found
+    // find insertion point
+    // Insertion sort
+    while (*current_entry_ptr != NULL) {
+        int cmp = strcmp((*current_entry_ptr)->key, key);
+        if (cmp == 0) {
+            // Key found
+            kv_node_t *new_node = malloc(sizeof(kv_node_t));
+            new_node->value = strdup(value);
+            // Prepend value for O(1) insertion
+            new_node->next = (*current_entry_ptr)->values;
+            (*current_entry_ptr)->values = new_node;
+            
+            pthread_mutex_unlock(&partitions[partition_num].partition_lock);
             return;
+        } else if (cmp > 0) {
+            break;
         }
-        entry = entry->next;
+        current_entry_ptr = &((*current_entry_ptr)->next);
     }
-    // In the case the bucket is empty
 
+    // If Key does not exist
     entry_t *new_entry = malloc(sizeof(entry_t));
-    new_entry->key = key;
-    new_entry->next = NULL;
-
+    new_entry->key = strdup(key);
+    new_entry->cursor = NULL;
+    new_entry->next = *current_entry_ptr;
+    
     kv_node_t *new_node = malloc(sizeof(kv_node_t));
     new_node->value = strdup(value);
     new_node->next = NULL;
     new_entry->values = new_node;
-
-    partitions[partition_num].buckets[bucket_num] = new_entry;
     
-    // Relase Lock
-
+    *current_entry_ptr = new_entry;
+    
+    pthread_mutex_unlock(&partitions[partition_num].partition_lock);
 }
 
 unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
